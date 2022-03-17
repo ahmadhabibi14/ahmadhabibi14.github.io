896e83742385bb83e6c26d8942134ef645c467e1
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-03-17 23:47:24 +0800

    Bissmillah PWA Amiiiiin

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var cacheName = 'hello-pwa';
-var filesToCache = [
+const CACHE_NAME = 'SW-001';
+const toCache = [
   '/',
   '/index.html',
   '/css/style.css',
@@ -7,24 +7,43 @@ var filesToCache = [
   '/css/font-awesome-6.0.0.css',
   '/css/syntax.css',
   '/js/font-awesome-6.0.0.js',
+  '/js/script.js',
   '/js/dark-mode.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(toCache)
+      })
+      .then(self.skipWaiting())
+  )
+})
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match(event.request)
+        })
     })
-  );
-});
+  )
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Hapus cache lama', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
